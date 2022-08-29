5811c83b90195d91b1f047b02d64d3cfb441ef75
Rafael Pires <rafaelbrpires@gmail.com>
2022-08-29 11:56:30 -0300

    ref text skills

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const cardSkills = document.querySelectorAll('.cards-skills')
 const description = document.querySelector('.description')
 
 const skills = {
-    html:'html é ...',
-    css:'css3 é ...',
-    javascript:'javascript é ...',
-    sass:'SASS é ...',
+    html:'HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais irnformações a página vai exibir.',
+    css:'CSS é uma linguagem de folhas de estilo composta por "camadas", criado com o propósito de estilizar as páginas HTML.',
+    javascript:'JavaScript é uma linguaguem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.',
+    sass:'Sass (pré-processador css) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass.',
     default:'*passe o cursor do mouse no card para ler*'
 }
 
